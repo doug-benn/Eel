4174932f45ac330ff579f9272a82a8f99125b4b9
doug-benn <fiery.coder@gmail.com>
2022-12-14 08:43:57 +0000

    Pass please?

--- tests/utils.py
+++ tests/utils.py
@@ -34,7 +34,7 @@ def get_process_listening_port(proc):
             conn = next(filter(lambda conn: conn.status == "LISTEN", child.connections()))
     else:
         psutil_proc = psutil.Process(proc.pid)
-        while psutil_proc.connections() == [] and not any(conn.status == "LISTEN" for conn in psutil_proc.connections()):
+        while not any(conn.status == "LISTEN" for conn in psutil_proc.connections()):
             time.sleep(0.01)
 
         conn = next(filter(lambda conn: conn.status == "LISTEN", psutil_proc.connections()))
